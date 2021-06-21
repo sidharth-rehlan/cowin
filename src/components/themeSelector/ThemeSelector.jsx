321be54bb60d56b970fa15856c54d3965bc4d335
sidharth <sidharth@nagarro.com>
2021-06-21 12:09:35 +0530

    implement switch selector

--- src/components/themeSelector/ThemeSelector.jsx
+++ src/components/themeSelector/ThemeSelector.jsx
@@ -1,21 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import Switch from "react-switch";
+
 import themeContext from "context/ThemeContext";
 import "./style.scss";
 
 function ThemeSelector() {
   const ctx = useContext(themeContext);
-  const onChangeHandler = (e) => {
-    const isChecked = e.target.checked;
-    ctx.setTheme(isChecked);
+
+  const [checked, setchecked] = useState(false);
+
+  const onChangeHandler = () => {
+    ctx.setTheme(checked);
+    setchecked(!checked);
   };
   return (
     <form className="darkThemeSelector-form">
-      <label htmlFor="themeSelector">Dark Theme</label>
-      <input
-        type="checkbox"
-        name="themeSelector"
-        id="themeSelector"
+      <label htmlFor="themeSelector">Theme</label>
+      <Switch
         onChange={onChangeHandler}
+        onColor="#ffa500"
+        offColor="#000"
+        checked={checked}
+        className="darkThemeSelector-switch"
       />
     </form>
   );
